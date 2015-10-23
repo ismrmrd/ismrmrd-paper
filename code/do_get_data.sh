4701f034074ac203ae481957ae897df9b6e090ec
Souheil Inati <souheil.inati@gmail.com>
2015-10-22 21:39:55 -0400

    Modified get data script to use curl and point to github.

--- code/do_get_data.sh
+++ code/do_get_data.sh
@@ -4,7 +4,7 @@
 ismrmrd_generate_cartesian_shepp_logan -m 512 -o synth.h5
 
 # Copy the scanner data here
-wget https://github.com/ismrmrd/ismrmrd/releases/download/v1.2.3-data/ismrmrd_data.zip
+curl -L -o ismrmrd_data.zip https://github.com/ismrmrd/ismrmrd/releases/download/v1.2.3-data/ismrmrd_data.zip
 unzip ismrmrd_data.zip
 find ismrmrd_data -name "*.h5" -exec cp {} . \; -print
 
